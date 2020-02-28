06840e6308c0125c04d257e4b1758f2f0e3d77fc
roonil-wazlib <elfhogan@gmail.com>
2020-02-29 02:30:18 +1300

    script now stretches image to fill entire background

--- set_desktop.sh
+++ set_desktop.sh
@@ -1,4 +1,8 @@
 python3 /home/user1/Desktop/xkcd_scrape/scrape_comic.py
 
+convert /home/user1/Desktop/xkcd_scrape/latest_comic.png -resize 1920x1080 /home/user1/Desktop/xkcd_scrape/latest_comic.png
+
+gsettings set org.gnome.desktop.background picture-options 'stretched'
+
 gsettings set org.gnome.desktop.background picture-uri file:///home/user1/Desktop/xkcd_scrape/latest_comic.png
 
