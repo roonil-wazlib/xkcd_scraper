44dbd8e7b5203cb21844740d72eb3bb22d2e8979
roonil-wazlib <elfhogan@gmail.com>
2020-02-29 01:46:10 +1300

    Updated to run via xkcd alias from any directory

--- set_desktop.sh
+++ set_desktop.sh
@@ -1,4 +1,4 @@
-python3 scrape_comic.py
+python3 /home/user1/Desktop/xkcd_scrape/scrape_comic.py
 
 gsettings set org.gnome.desktop.background picture-uri file:///home/user1/Desktop/xkcd_scrape/latest_comic.png
 
