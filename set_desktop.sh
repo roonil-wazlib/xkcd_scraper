2d1ec7aac8299360c38e6bbf747c3770912c71b3
Emma Hogan <emma.hogan314@gmail.com>
2020-06-28 00:43:38 +1200

    Update set_desktop.sh

--- set_desktop.sh
+++ set_desktop.sh
@@ -1,11 +1,11 @@
-python3 /home/user1/Desktop/xkcd_scrape/scrape_comic.py
+python3 FILE_PATH/scrape_comic.py
 
-convert /home/user1/Desktop/xkcd_scrape/latest_comic.png -resize 1920x1080 /home/user1/Desktop/xkcd_scrape/latest_comic.png
+convert FILE_PATH/latest_comic.png -resize 1920x1080 FILE_PATH/latest_comic.png
 
 gsettings set org.gnome.desktop.background picture-options 'stretched'
 
-gsettings set org.gnome.desktop.background picture-uri file:///home/user1/Desktop/xkcd_scrape/latest_comic.png
+gsettings set org.gnome.desktop.background picture-uri file://FILE_PATH/latest_comic.png
 
 gsettings set org.gnome.desktop.background show-desktop-icons false
 gsettings set org.gnome.shell.extensions.dash-to-dock intellihide true
-bash /home/user1/Desktop/xkcd_scrape/set_mouseover.sh
+bash FILE_PATH/set_mouseover.sh
